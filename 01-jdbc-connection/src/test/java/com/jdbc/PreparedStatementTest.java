5cc5ffd4c017fae396176ce20885f7291ca8b72e
hewenyu <he875027547@126.com>
2025-06-16 23:39:09 +0800

    修改登录单元测试

--- 01-jdbc-connection/src/test/java/com/jdbc/PreparedStatementTest.java
+++ 01-jdbc-connection/src/test/java/com/jdbc/PreparedStatementTest.java
@@ -4,18 +4,12 @@ import com.example.jdbc.bean.User;
 import com.example.jdbc.utils.PreparedStatementUtil;
 import org.junit.Test;
 
-import java.util.Scanner;
-
 public class PreparedStatementTest {
 
     @Test
     public void testLogin() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("请输入用户名：");
-        String user = scanner.nextLine();
-        System.out.print("请输入密码：");
-        String password = scanner.nextLine();
+        String user = "AA";
+        String password = "123456";
         //SELECT user,password FROM user_table WHERE user = '1' or ' AND password = '=1 or '1' = '1'
         String sql = "SELECT user,password FROM user_table WHERE user = ? and password = ?";
         User userBean = PreparedStatementUtil.getInstance(User.class, sql, user, password);
